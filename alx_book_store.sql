3bea1b9d299d461760507a8b855b37962d700ecd
David Oduro Aboagye <your-github-email@example.com>
2025-12-23 15:16:37 +0000

    first commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,11 +1,39 @@
-import mysql.connector
+create Database alx_book_store;
+USE alx_book_store;
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",              # <-- CHANGE THIS
-    password="ASascom619$$", # <-- CHANGE THIS
-    database="alx_book_store" # <-- OR ANY DATABASE YOU CREATED
-)
+CREATE TABLE AUTHORS (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
 
-print("Connected to MySQL Server Version:")
-print(mydb.get_server_info())
+CREATE TABLE BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+CREATE TABLE CUSTOMERS (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE ORDERS (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+CREATE TABLE ORDER_DETAILS (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
