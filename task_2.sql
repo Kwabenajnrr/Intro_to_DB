4a665f0d42227fab4cd109410962c49168781540
David Oduro Aboagye <your-github-email@example.com>
2025-12-30 08:40:48 +0000

    fourth commit

--- task_2.sql
+++ task_2.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Authors (
     PRIMARY KEY (author_id)
 );
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS customers (
     PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     order_detail_id INT AUTO_INCREMENT,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
